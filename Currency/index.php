36f373dcd5056c56d7553c889d0137f689f397c0
Mauricio Da Graca <maurofm1992@gmail.com>
2017-04-30 19:30:12 -0400

    Add files via upload

---
+++ Currency/index.php
@@ -0,0 +1,70 @@
+<?php
+function currencyConverter($currency_from,$currency_to,$currency_input){
+    
+    
+    $amount = urlencode($currency_input);
+    $from_Currency = urlencode($currency_from);
+    $to_Currency = urlencode($currency_to);
+    $get = file_get_contents("https://www.google.com/finance/converter?a=$amount&from=$from_Currency&to=$to_Currency");
+    $get = explode("<span class=bld>",$get);
+    $get = explode("</span>",$get[1]);  
+    $currency_output = preg_replace("/[^0-9\.]/", null, $get[0]);
+    
+    return $currency_output;
+}
+
+$display_result = false;
+$currency_from = "";
+$currency_to = "";
+$currency_input = 1;
+
+if (isset($_POST['currency_from']) && isset($_POST['currency_to']) && isset($_POST['currency_input']))
+{
+	$currency_from = $_POST['currency_from'];
+	$currency_to = $_POST['currency_to'];
+	$currency_input = $_POST['currency_input'];
+ 
+    $currency = currencyConverter($currency_from,$currency_to,$currency_input);
+    $display_result = true;
+}
+
+echo <<<_END
+<html>
+	<head>
+		<title>Currency conversion</title>
+        <link rel="stylesheet" href="css/Style.css"/>
+	</head>
+	<body>
+    <h1> Welcome to the Currency convertor </h1>
+    <p> This is a currency converter that is done using the programming language of php.</p>
+    <p>Make sure to convert every time you can since the value changes every second that passes.</p>
+    <div class ="cube">
+    <div class ="main">
+	<form method="post" action="currency5.php">
+		<label>Enter amount:</label>
+		<input type="text" name="currency_input" />
+        <label>Select currency (from):</label>
+        <select name="currency_from">
+            <option value="USD">US Dollar</option>
+            <option value="EUR">Euro</option>
+            <option value="INR">Indian Rupee</option>
+        </select>
+        <br>
+        <label>Select currency (to):</label>
+        <select name="currency_to">
+            <option value="USD">US Dollar</option>
+            <option value="EUR">Euro</option>
+            <option value="INR">Indian Rupee</option>
+        </select>
+		<input type="submit" value="Convert!" />
+        <p>$currency_input $currency_from is equal to $currency $currency_to</p>
+	</form>
+    </div>
+    </div>
+	</body>
+</html>
+_END;
+
+
+?>
+
